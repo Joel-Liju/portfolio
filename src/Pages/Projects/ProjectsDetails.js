679937a80839af2bc049ebc083df324ba1091022
Dare <joelliju10@gmail.com>
2022-05-14 07:22:22 -0400

    added imagers

---
+++ src/Pages/Projects/ProjectsDetails.js
@@ -0,0 +1,27 @@
+const Projects =[
+    {
+        "name":"Brock Chat Bot",
+        "desc":"This is a chat bot which answers simple questions from the user, where we accomplished this using software development methodology like Scrum",
+        "toolsused":"React, Flask, Docker, Selenium, PyTest, Python",
+        "imageURL":"/pictures/chatbot.png"
+    },
+    {
+        "name":"Chess",
+        "desc":"The objective of the project was to create a Chess game using AI, we accomplished this using Alpha-Beta Pruning algorithm",
+        "toolsused":"Java, Java Swing, GitHub",
+        "imageURL":"/pictures/agile.jpg"
+    },
+    {
+        "name":"Burger Tutorial",
+        "desc":"The objective of this project was to create a animation using Blender, and this was done with the help of Kam Sadiq, where we made most of the models and animated / edited the entire video.",
+        "toolsused":"Blender, FL Studio, Audacity",
+        "imageURL":"/pictures/burger.png"
+    },
+    {
+        "name":"Sketchbook App",
+        "desc":"This project, we created a mobile application for a local Comic book shop as a team, which included Brendan Park, Dalton Comer",
+        "toolsused":"Java, Android Studio",
+        "imageURL":"/pictures/sketchbook.jpg"
+    }
+];
+export default Projects;
