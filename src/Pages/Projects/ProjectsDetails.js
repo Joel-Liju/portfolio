f7d5d2b633220b7879287646c80bea21e1a62899
Dare <joelliju10@gmail.com>
2022-05-18 17:53:16 -0400

    file path

--- src/Pages/Projects/ProjectsDetails.js
+++ src/Pages/Projects/ProjectsDetails.js
@@ -3,37 +3,37 @@ const Projects =[
         "name":"Brock Chat Bot",
         "desc":"This is a chat bot which answers simple questions from the user. We accomplished this using software development methodology like Scrum, and different tools that were available.",
         "toolsused":"React, Flask, Docker, Selenium, PyTest, Python",
-        "imageURL":"/pictures/chatbot.png"
+        "imageURL":"/portfolio/pictures/chatbot.png"
     },
     {
         "name":"Chess",
         "desc":"The objective of the project was to create a Chess game using AI, we accomplished this using Alpha-Beta Pruning algorithm.",
         "toolsused":"Java, Java Swing, GitHub",
-        "imageURL":"/pictures/chess.png"
+        "imageURL":"/portfolio/pictures/chess.png"
     },
     {
         "name":"Burger Tutorial",
         "desc":"The objective of this project was to create a animation using Blender, and this was done with the help of Kam Sadiq, where we made most of the models and animated / edited the entire video.",
         "toolsused":"Blender, FL Studio, Audacity",
-        "imageURL":"/pictures/burger.png"
+        "imageURL":"/portfolio/pictures/burger.png"
     },
     {
         "name":"Sketchbook App",
         "desc":"This project, we created a mobile application for a local Comic book shop as a team, which included Brendan Park, Dalton Comer",
         "toolsused":"Java, Android Studio",
-        "imageURL":"/pictures/sketchbook.jpg"
+        "imageURL":"/portfolio/pictures/sketchbook.jpg"
     },
     {
         "name":"Analysis of Tweets",
         "desc":"Worked on analysing tweets from the prime minister of Canada and President of US, to predict the forex market.",
         "toolsused":"Selenium, Scikit Learn",
-        "imageURL":"/pictures/tweets.png"
+        "imageURL":"/portfolio/pictures/tweets.png"
     },
     {
         "name":"GitHub Parser",
         "desc":"For Hack The North, our team created a chrome extension that parsers through a give GitHub repo's Read Me and gives a side bar for you to navigate the project.",
         "toolsused":"Javascript",
-        "imageURL":"/pictures/githubparser.png"
+        "imageURL":"/portfolio/pictures/githubparser.png"
     }
 ];
 export default Projects;
